d7d43468bda33339800e2c24668b866cb687b952
Cristian Ayala Sánchez <ayalasanchezcristian2000@gmail.com>
2024-07-24 21:08:37 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
 };
+
+function randomCard() {
+  const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "A", "Q"];
+  const suits = ["♦", "♥", "♠", "♣"];
+  const suitColors = {
+    "♦": "red",
+    "♥": "red",
+    "♠": "black",
+    "♣": "black"
+  };
+
+  const randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+
+  const randomNumber = numbers[randomNumberIndex];
+  const randomSuit = suits[randomSuitIndex];
+  const suitColor = suitColors[randomSuit];
+
+  document.getElementById("number").textContent = randomNumber;
+  document.getElementById("topSuit").textContent = randomSuit;
+  document.getElementById("bottomSuit").textContent = randomSuit;
+
+  document.getElementById("topSuit").style.color = suitColor;
+  document.getElementById("bottomSuit").style.color = suitColor;
+
+}
+
+document.getElementById("reloadButton").addEventListener("click", function() {
+  randomCard();
+});
